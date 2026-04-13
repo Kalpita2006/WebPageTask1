626324aed538b3a78ca0ce352301e9f502b94ec5
Kalpita tarkar <kalpitantarkar@gmail.com>
2026-04-13 16:42:20 +0530

    First commit - Basics of Web Development webpage

---
+++ Desktop/WebPageTask1/script.js
@@ -0,0 +1,6 @@
+
+    document.getElementById("messageBtn").onclick = function () {
+
+        alert("Welcome! This webpage explains the basics of Web Development using HTML, CSS and JavaScript.");
+
+    };
